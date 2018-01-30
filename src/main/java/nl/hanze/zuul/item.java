5c734e02afbfdc5ec80531fc10a438145950f528
“dgroen83“ <david.groen@duo.nl>
2018-01-30 21:21:23 +0100

    klasse item toegevoegd aan het project. incl. getters en setters.

---
+++ src/main/java/nl/hanze/zuul/item.java
@@ -0,0 +1,31 @@
+package nl.hanze.zuul;
+
+public class item {
+    private String name;
+    private String description;
+    private int weigth;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getWeigth() {
+        return weigth;
+    }
+
+    public void setWeigth(int weigth) {
+        this.weigth = weigth;
+    }
+}
